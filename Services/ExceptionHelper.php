8258380ecd2974e5b55fde5496aeac244c6a92d1
Pascal Rossa <rossa@creatissimo.net>
2017-10-25 11:22:01 +0200

    prevent cropping in exception trace as string

--- Services/ExceptionHelper.php
+++ Services/ExceptionHelper.php
@@ -64,7 +64,7 @@ class ExceptionHelper
         $attachment->addField(new AttachmentField('Timestamp', $now->format(DATE_ISO8601), true));
 
         if ($trace || $this->shouldAddTrace()) {
-            $attachment->addField(new AttachmentField('Trace', $exception->getTraceAsString()));
+            $attachment->addField(new AttachmentField('Trace', $this->getExceptionTraceAsString($exception)));
         }
 
         $mmMessage->addAttachment($attachment);
@@ -131,5 +131,50 @@ class ExceptionHelper
         }
 
         return $shouldProcess;
+    }
+
+
+    /**
+     * @param \Exception $exception
+     *
+     * @return string
+     */
+    private function getExceptionTraceAsString(\Exception $exception)
+    {
+        $rtn   = "";
+        $count = 0;
+        foreach ($exception->getTrace() as $frame) {
+            $args = "";
+            if (isset($frame['args'])) {
+                $args = [];
+                foreach ($frame['args'] as $arg) {
+                    if (is_string($arg)) {
+                        $args[] = "'" . $arg . "'";
+                    } elseif (is_array($arg)) {
+                        $args[] = "Array";
+                    } elseif (is_null($arg)) {
+                        $args[] = 'NULL';
+                    } elseif (is_bool($arg)) {
+                        $args[] = ($arg) ? "true" : "false";
+                    } elseif (is_object($arg)) {
+                        $args[] = get_class($arg);
+                    } elseif (is_resource($arg)) {
+                        $args[] = get_resource_type($arg);
+                    } else {
+                        $args[] = $arg;
+                    }
+                }
+                $args = join(", ", $args);
+            }
+            $rtn .= sprintf("#%s %s(%s): %s(%s)\n",
+                $count,
+                isset($frame['file']) ? $frame['file'] : 'unknown file',
+                isset($frame['line']) ? $frame['line'] : 'unknown line',
+                (isset($frame['class'])) ? $frame['class'] . $frame['type'] . $frame['function'] : $frame['function'],
+                $args);
+            $count++;
+        }
+
+        return $rtn;
     }
 }
